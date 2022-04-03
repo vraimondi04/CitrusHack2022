44617ad859371f7b6d5c0ae985f2a9ddbefc44fe
vraimondi04 <42946121+vraimondi04@users.noreply.github.com>
2022-04-03 00:54:36 -0700

    Created first instance of marker and output
    -Marker for the bell tower created --> including the PNG file, location, and pixel size
    -Loop to traverse through all eventual markers created
    -Animation for the drop effect for each marker added (line 34)

--- script.js
+++ script.js
@@ -7,4 +7,40 @@ function initMap(){
     fullscreenControl: false,
     streetViewControl: false
     });
+  
+  const markers = [
+    [
+      "Bell Tower",
+      33.973345705203826,
+      -117.32818406220579,
+      "Bell_Tower.png",
+      180,
+      180
+    ],
+  ];
+  
+  for (let i = 0; i < markers.length; i++) {
+    const currMarker = markers[i];
+    const marker = new google.maps.Marker({
+    position: { lat: currMarker[1], lng: currMarker[2] },
+    map,
+    title: currMarker[0],
+    icon: {
+      url: currMarker[3],
+      scaledSize: new google.maps.Size(currMarker[4], currMarker[5]), 
+      // origin: new google.maps.Point(0,0), // origin
+      // anchor: new google.maps.Point(0, 0) // anchor
+    }, 
+      animation: google.maps.Animation.DROP
+    });
+  
+    const infowindow = new google.maps.InfoWindow({
+      content: currMarker[0],
+    });
+  
+    marker.addListener("click", () =>{
+      infowindow.open(map, marker);
+    });
+  }
+}
 

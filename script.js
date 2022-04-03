c91055a7bc13fe062d0dd4b9ae7db8540634f1ff
vraimondi04 <42946121+vraimondi04@users.noreply.github.com>
2022-04-03 00:13:40 -0700

    Started initialize map function to output google maps
    -initialize map on the location of UCR through longitude and latitude values 
    -from Google Cloud API, copying the mapID of the previously edited map

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function initMap(){
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 33.97340039276367, lng: -117.32842702670901},
+    zoom: 16,
+    mapId: '5710fdbd76ee3a9c', 
+    mapTypeControl: false,
+    fullscreenControl: false,
+    streetViewControl: false
+    });
 
